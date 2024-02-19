fb5eb1f70550552ad21d45719e6088f3fe945780
Roko Parac <rp218@ic.ac.uk>
2024-02-19 11:34:47 +0000

    Adapts dependencies to work with python=3.8

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_subgoal_automata',
       version='0.0.3',
-      install_requires=['gym==0.26.2', 'numpy==1.26.4', 'pygame==1.9.6', 'graphviz==0.14']
+      install_requires=['gym==0.26.2', 'numpy==1.24.4', 'pygame==1.9.6', 'graphviz==0.14']
 )
