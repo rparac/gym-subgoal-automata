503d13735e8f6467ae8b5c9b8438d91af615b0d4
Roko Parac <roko.parac@gmail.com>
2025-02-27 10:33:49 +0100

    Update setup.py
    Change version to avoid pip conflicts

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_subgoal_automata',
-      version='0.0.3',
+      version='0.0.4',
       install_requires=['gymnasium>=0.26.0', 'numpy==1.26.4', 'pygame==2.5.0', 'graphviz==0.14']
 )
